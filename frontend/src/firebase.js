5210dab94edc6993ebfc34da30fc303854dcded5
Seunggeun Kim <daeryak0406@icloud.com>
2025-04-17 14:31:11 +0900

    (M) 25.04.17. 웹 상 배포 버전

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -10,7 +10,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
-// console.log("API KEY:", process.env.REACT_APP_FIREBASE_API_KEY); // 이게 undefined이면 .env 안 들어간 거
 
 const app = initializeApp(firebaseConfig);
 
